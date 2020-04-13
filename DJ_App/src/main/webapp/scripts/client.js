0a53bccbb8958e25b0542b1b84796402dd184692
anooj-shah <thisisanooj@gmail.com>
2020-04-13 11:46:08 -0500

    Working version

--- DJ_App/src/main/webapp/scripts/client.js
+++ DJ_App/src/main/webapp/scripts/client.js
@@ -1,49 +1,79 @@
-async function getEvents() {
-    const response = await fetch('/events');
-    const events = await response.json();
-
-    const accessID = ""
-    function verifyAccessCode() {
-        const verifiedAccess = document.getElementById('verified-access')
-        const accessCode = document.getElementById('access-code')
-        const eventName = document.getElementById('eventName');
-        const eventID = document.getElementById('eventID');
-        const eventDJ = document.getElementById('eventDJ');
-        const errorMsg = document.getElementById("errorMsg")
-        accessID = accessCode.value;
-
-        events.forEach((event) => {
-            if (event.eventID === accessID){
-                eventID.innerText = event.eventID
-                eventName.innerText = event.eventName
-                eventDJ.innerText = event.eventDJ
-            }
-            else{
-                errorMsg.innerText = "incorrect access code, please enter access code"
-            }
-            console.log(event.eventName);
-        });
-        verifiedAccess.innerText = "clientScreen.html"
-        console.log(accessID);
+function getEvents() {
+  fetch('/events').then(response => response.json()).then((events) => {
+    const eventsElement = document.getElementById('events-container');
+    eventsElement.innerHTML = "";
+    console.log(events);
+    for (i of events) {
+      eventsElement.appendChild(createListElement(i.eventName));
     }
+  });
+}
+
+function getSongs() {
+  fetch('/songs').then(response => response.json()).then((songs) => {
+    const songsElement = document.getElementById('songs-container');
+    songsElement.innerHTML = "";
+    console.log(songs);
+    for (i of songs) {
+      songsElement.appendChild(createListElement(i.songName));
+    }
+  });
+}
+
+function createListElement(event) {
+  const element = document.createElement('li');
+  element.innerText = event;
+  return element;
+}
+
+
+
+// async function getEvents() {
+//     const response = await fetch('/events');
+//     const events = await response.json();
+
+//     const accessID = ""
+//     function verifyAccessCode() {
+//         const verifiedAccess = document.getElementById('verified-access')
+//         const accessCode = document.getElementById('access-code')
+//         const eventName = document.getElementById('eventName');
+//         const eventID = document.getElementById('eventID');
+//         const eventDJ = document.getElementById('eventDJ');
+//         const errorMsg = document.getElementById("errorMsg")
+//         accessID = accessCode.value;
+
+//         events.forEach((event) => {
+//             if (event.eventID === accessID){
+//                 eventID.innerText = event.eventID
+//                 eventName.innerText = event.eventName
+//                 eventDJ.innerText = event.eventDJ
+//             }
+//             else{
+//                 errorMsg.innerText = "incorrect access code, please enter access code"
+//             }
+//             console.log(event.eventName);
+//         });
+//         verifiedAccess.innerText = "clientScreen.html"
+//         console.log(accessID);
+//     }
 
     
-}
+// }
 
-async function getSongs() {
-    const response = await fetch('/songs');
-    const songs = await response.json();
+// async function getSongs() {
+//     const response = await fetch('/songs');
+//     const songs = await response.json();
 
-    const songList = document.getElementById('song-list');
-    songs.forEach((song) => {
-        songList.appendChild(createListElement(song.songName));
-        console.log(song.songName);
-    });  
-}
+//     const songList = document.getElementById('song-list');
+//     songs.forEach((song) => {
+//         songList.appendChild(createListElement(song.songName));
+//         console.log(song.songName);
+//     });  
+// }
 
-function createListElement(text) {
-        const liElement = document.createElement('li');
-        liElement.innerText = text;
-        console.log(text);
-        return liElement;
-    }
+// function createListElement(text) {
+//         const liElement = document.createElement('li');
+//         liElement.innerText = text;
+//         console.log(text);
+//         return liElement;
+//     }
