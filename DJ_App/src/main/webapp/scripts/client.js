47646c1aede83d7f9acf3213b5f96b8c196137d0
utibeno <prinzuty@gmail.com>
2020-04-17 12:10:14 -0400

    javascript for loading from datastore

--- DJ_App/src/main/webapp/scripts/client.js
+++ DJ_App/src/main/webapp/scripts/client.js
@@ -1,79 +1,96 @@
-function getEvents() {
-  fetch('/events').then(response => response.json()).then((events) => {
-    const eventsElement = document.getElementById('events-container');
-    eventsElement.innerHTML = "";
-    console.log(events);
-    for (i of events) {
-      eventsElement.appendChild(createListElement(i.eventName));
-    }
-  });
-}
-
-function getSongs() {
-  fetch('/songs').then(response => response.json()).then((songs) => {
-    const songsElement = document.getElementById('songs-container');
-    songsElement.innerHTML = "";
-    console.log(songs);
-    for (i of songs) {
-      songsElement.appendChild(createListElement(i.songName));
-    }
-  });
-}
-
-function createListElement(event) {
-  const element = document.createElement('li');
-  element.innerText = event;
-  return element;
-}
-
-
-
-// async function getEvents() {
-//     const response = await fetch('/events');
-//     const events = await response.json();
-
-//     const accessID = ""
-//     function verifyAccessCode() {
-//         const verifiedAccess = document.getElementById('verified-access')
-//         const accessCode = document.getElementById('access-code')
-//         const eventName = document.getElementById('eventName');
-//         const eventID = document.getElementById('eventID');
-//         const eventDJ = document.getElementById('eventDJ');
-//         const errorMsg = document.getElementById("errorMsg")
-//         accessID = accessCode.value;
-
-//         events.forEach((event) => {
-//             if (event.eventID === accessID){
-//                 eventID.innerText = event.eventID
-//                 eventName.innerText = event.eventName
-//                 eventDJ.innerText = event.eventDJ
-//             }
-//             else{
-//                 errorMsg.innerText = "incorrect access code, please enter access code"
-//             }
-//             console.log(event.eventName);
-//         });
-//         verifiedAccess.innerText = "clientScreen.html"
-//         console.log(accessID);
+// function getEvents() {
+//   fetch('/events').then(response => response.json()).then((events) => {
+//     const eventsElement = document.getElementById('events-container');
+//     eventsElement.innerHTML = "";
+//     console.log(events);
+//     for (i of events) {
+//       eventsElement.appendChild(createListElement(i.eventName));
 //     }
+//   });
+// }
+
+// function getSongs() {
+//   fetch('/songs').then(response => response.json()).then((songs) => {
+//     const songsElement = document.getElementById('songs-container');
+//     songsElement.innerHTML = "";
+//     console.log(songs);
+//     for (i of songs) {
+//       songsElement.appendChild(createListElement(i.songName));
+//     }
+//   });
+// }
+
+// function createListElement(event) {
+//   const element = document.createElement('li');
+//   element.innerText = event;
+//   return element;
+// }
+
+
+
+async function getEvents() {
+    const response = await fetch('/events');
+    const events = await response.json();
+
+    const accessID = ""
+    // function verifyAccessCode() {
+    //     const verifiedAccess = document.getElementById('verified-access')
+        const accessCode = document.getElementById('access-code').value
+        console.log(accessCode);
+
+        const eventName = document.getElementById('eventName');
+        const eventID = document.getElementById('eventID');
+        const eventDJ = document.getElementById('eventDJ');
+        const errorMsg = document.getElementById("errorMsg")
+    //     accessID = accessCode.value;
+
+        count = 0
+        events.forEach((event) => {
+            if (event.eventID === accessCode){
+                console.log(count); // if statement not running, tonights stop
+                fillUserPage()
+                window.location.assign = 'https://8080-dot-10844361-dot-devshell.appspot.com/userPage.html'
+            }
+            else{
+                if (events.length < count){
+                    count++
+                    errorMsg.innerText = "incorrect access code, please enter access code"
+                    return
+                }
+            }
+            
+        });
+
+        function fillUserPage() {
+            const eventName = document.getElementById('eventName');
+            const eventID = document.getElementById('eventID');
+            const eventDJ = document.getElementById('eventDJ');
+
+            eventID.innerText = event.eventID
+            eventName.innerText = event.eventName
+            eventDJ.innerText = event.eventDJ
+        }
+        // verifiedAccess.innerText = "clientScreen.html"
+        console.log(events);
+    }
 
     
 // }
 
-// async function getSongs() {
-//     const response = await fetch('/songs');
-//     const songs = await response.json();
+async function getSongs() {
+    const response = await fetch('/songs');
+    const songs = await response.json();
 
-//     const songList = document.getElementById('song-list');
-//     songs.forEach((song) => {
-//         songList.appendChild(createListElement(song.songName));
-//         console.log(song.songName);
-//     });  
-// }
+    const songList = document.getElementById('song-list');
+    songs.forEach((song) => {
+        songList.appendChild(createListElement(song.songName));
+        console.log(song.songName);
+    });  
+}
 
-// function createListElement(text) {
-//         const liElement = document.createElement('li');
-//         liElement.innerText = text;
-//         console.log(text);
-//         return liElement;
-//     }
+function createListElement(text) {
+        const liElement = document.createElement('li');
+        liElement.innerText = text;
+        console.log(text);
+        return liElement;
+    }
