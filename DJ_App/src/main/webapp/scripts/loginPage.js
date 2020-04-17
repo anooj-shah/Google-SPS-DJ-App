cca22d95131ecc3d156bf4aebcfb635320ddbd69
anooj-shah <thisisanooj@gmail.com>
2020-04-17 10:14:57 -0500

    home page

--- DJ_App/src/main/webapp/scripts/loginPage.js
+++ DJ_App/src/main/webapp/scripts/loginPage.js
@@ -9,3 +9,16 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function createEventForm() {
+    var r = document.createElement('span');
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Email");
+    y.setAttribute("Name", "textelement_" + i);
+    r.appendChild(y);
+    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+    r.appendChild(g);
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("createEventForm").appendChild(r);
+}
